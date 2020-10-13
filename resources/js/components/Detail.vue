afa2de652f2f20471101f5ee422f42c3e10cb901
jhonnstark <72222544+jhonnstark@users.noreply.github.com>
2020-10-12 22:29:22 -0500

    500mb

--- resources/js/components/Detail.vue
+++ resources/js/components/Detail.vue
@@ -62,7 +62,7 @@
         <div v-if="file && doc.includes(file.ext)">
             <iframe :src="'https://view.officeapps.live.com/op/embed.aspx?src=' + encodeURIComponent(file.url)"
                     width='100%'
-                    height='100%'
+                    height='900px'
                     frameborder='0'>
             </iframe>
         </div>
