af56efd44815556702d2566bb6f28b58b6ba1801
jhonnstark <72222544+jhonnstark@users.noreply.github.com>
2020-10-29 19:40:34 -0600

    score component

---
+++ resources/js/components/form/ScoreMaterial.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="col-md-6">
+        hrfhfg
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ScoreMaterial",
+    props: ['id', 'edit'],
+    created () {
+        console.log(this.id)
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
