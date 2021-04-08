2c1201cbf8900e6ef497aa109c96c509b236a25e
jhonnstark <72222544+jhonnstark@users.noreply.github.com>
2021-04-08 15:49:08 -0500

    save claims

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'can.exam' => \App\Http\Middleware\ContinueExam::class,
+        'can.claim' => \App\Http\Middleware\ContinueClaim::class,
     ];
 }
