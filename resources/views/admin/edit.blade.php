752b1fec54e76e3f93c22dda00217aa393a9bd53
Jonathan Cervantes <jonathan.cervantes@printerlogic.com>
2022-09-17 14:10:54 -0500

    prefill levels and programs

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -79,7 +79,15 @@
     @if($role === 'teacher' || $role === 'user' || $role === 'level')
     <div class="container">
         <div class="row justify-content-between">
-              <h4>{{ __('course') }}</h4>
+            <h4>{{ __('course') }}</h4>
+            @if($role === 'level')
+                <div class="col-3 text-right">
+                    <a class="btn btn-primary btn-lg" href="{{ route('admin.course.register', ['level' => $id]) }}" role="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="21" height="21" fill="white"><path fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"></path></svg>
+                        Nuevo
+                    </a>
+                </div>
+            @endif
             <hr class="col-12">
         </div>
     </div>
@@ -87,6 +95,12 @@
         <div class="container">
             <div class="row justify-content-between">
                 <h4>{{ __('subject') }}</h4>
+                <div class="col-3 text-right">
+                    <a class="btn btn-primary btn-lg" href="{{ route('admin.subject.register', ['program' => $id]) }}" role="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="21" height="21" fill="white"><path fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.75 4.75a.75.75 0 00-1.5 0v2.5h-2.5a.75.75 0 000 1.5h2.5v2.5a.75.75 0 001.5 0v-2.5h2.5a.75.75 0 000-1.5h-2.5v-2.5z"></path></svg>
+                        Nuevo
+                    </a>
+                </div>
                 <hr class="col-12">
             </div>
         </div>
