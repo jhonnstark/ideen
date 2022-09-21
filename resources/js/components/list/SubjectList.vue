cea6d7629a177c69e671cb2bbebd27306d2fdd40
Jonathan Cervantes <jonathan.cervantes@printerlogic.com>
2022-09-21 14:35:37 -0500

    Tabla de materias

--- resources/js/components/list/SubjectList.vue
+++ resources/js/components/list/SubjectList.vue
@@ -5,14 +5,16 @@
             <table class="table table-striped">
                 <thead class="thead-dark">
                 <tr>
-                    <th scope="col">#</th>
+                    <th scope="col">Clave</th>
                     <th scope="col">Nombre</th>
+                    <th scope="col">Cuatrimestre</th>
+                    <th scope="col">Periodo</th>
                     <th scope="col" class="text-right">Acciones</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr v-if="isLoading">
-                    <td v-for="n in 3" class="text-center">
+                    <td v-for="n in 5" class="text-center">
                         <svg class="spin loader" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M8 2.5a5.487 5.487 0 00-4.131 1.869l1.204 1.204A.25.25 0 014.896 6H1.25A.25.25 0 011 5.75V2.104a.25.25 0 01.427-.177l1.38 1.38A7.001 7.001 0 0114.95 7.16a.75.75 0 11-1.49.178A5.501 5.501 0 008 2.5zM1.705 8.005a.75.75 0 01.834.656 5.501 5.501 0 009.592 2.97l-1.204-1.204a.25.25 0 01.177-.427h3.646a.25.25 0 01.25.25v3.646a.25.25 0 01-.427.177l-1.38-1.38A7.001 7.001 0 011.05 8.84a.75.75 0 01.656-.834z"></path></svg>
                     </td>
                 </tr>
@@ -24,8 +26,10 @@
                 </tr>
 
                 <tr v-else v-for="item in items" :key="item.id">
-                        <th scope="row">{{ item.id }}</th>
+                        <th scope="row">{{ item.code }}</th>
                         <td>{{ item.name }}</td>
+                    <td>{{ item.quarter }}ª</td>
+                        <td>{{ item.period }}ª</td>
                         <td class="text-right">
                             <a :href="'/' + type + '/subject/edit/' + item.id" type="submit" class="btn btn-primary">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M11.013 1.427a1.75 1.75 0 012.474 0l1.086 1.086a1.75 1.75 0 010 2.474l-8.61 8.61c-.21.21-.47.364-.756.445l-3.251.93a.75.75 0 01-.927-.928l.929-3.25a1.75 1.75 0 01.445-.758l8.61-8.61zm1.414 1.06a.25.25 0 00-.354 0L10.811 3.75l1.439 1.44 1.263-1.263a.25.25 0 000-.354l-1.086-1.086zM11.189 6.25L9.75 4.81l-6.286 6.287a.25.25 0 00-.064.108l-.558 1.953 1.953-.558a.249.249 0 00.108-.064l6.286-6.286z"></path></svg>
